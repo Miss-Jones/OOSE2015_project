81f3839ee2c1fa96da39c882b5884670572bdd55
lil-phil <philipojelvard@live.dk>
2015-05-06 19:45:14 +0200

    created overrided movement methods from the entity class to the player class

--- src/Pacman/Player.java
+++ src/Pacman/Player.java
@@ -1,5 +1,7 @@
 package Pacman;
 import org.newdawn.slick.Sound;
+import org.newdawn.slick.tiled.TiledMap;
+import org.newdawn.slick.util.pathfinding.AStarPathFinder;
  
  
 public class Player extends Entity {
@@ -30,5 +32,66 @@ public class Player extends Entity {
        
         public int GetScore(){
                 return this.score;
+        }
+       
+        public void movePath(Level map){
+                if(this.path.getLength()>this.mover){
+                        if(this.path.getX(this.mover)>Math.round(this.GetPosX()/32*8)/8f){
+                                this.MoveRight();
+                                this.SetSprite(this.GetAniRight());
+                        }else if(this.path.getX(this.mover)<Math.round(this.GetPosX()/32*8)/8f){
+                                this.MoveLeft();
+                                this.SetSprite(this.GetAniLeft());
+                        }else if(this.path.getY(this.mover)>Math.round(this.GetPosY()/32*8)/8f){
+                                this.MoveDown();
+                                this.SetSprite(this.GetAniDown());
+                        }else if(this.path.getY(this.mover)<Math.round(this.GetPosY()/32*8)/8f){
+                                this.MoveUp();
+                                this.SetSprite(this.GetAniUp());
+                        }
+                        if(this.path.getX(this.mover)==Math.round(this.GetPosX()/32*8)/8f&&this.path.getY(this.mover)==Math.round(this.GetPosY()/32*8)/8f){
+ 
+                                this.mover++;
+                                this.StopLeftRight();
+                                this.StopUpDown();
+                        }
+                }else{
+                        this.posX=this.path.getX(this.mover-1)*32;
+                        this.posY=this.path.getY(this.mover-1)*32;
+                }
+        }
+       
+        public void SetTargetPos(TiledMap map, int collisionLayerId){
+                this.TargetposY = 14;
+                this.TargetposX = 16;
+       
+}
+        public void SetPath(Level map){
+                this.SetTargetPos(map.GetMap(),map.GetIndexLayer());
+        this.map = new SimpleMap(map.GetMap(), map.GetIndexLayer());
+        this.pathFinder = new AStarPathFinder(this.map, this.maxPathLength, false);
+        this.path = this.pathFinder.findPath(null, (int)this.GetPosX()/32, (int)this.GetPosY()/32, this.TargetposX, this.TargetposY);
+        this.mover = 1;
+        }
+       
+        public void GoStepRight(TiledMap map, int moveableLayerId){
+                if(map.getTileId((int)(this.posX/32)+1, (int)this.posY/32, moveableLayerId)!=0){
+                this.path.appendStep((int)(this.posX/32)+1, (int)this.posY/32);
+                }
+        }
+        public void GoStepLeft(TiledMap map, int moveableLayerId){
+                if(map.getTileId((int)(this.posX/32)-1, (int)this.posY/32, moveableLayerId)!=0){
+                this.path.appendStep((int)(this.posX/32)-1, (int)this.posY/32);
+                }
+        }
+        public void GoStepUp(TiledMap map, int moveableLayerId){
+                if(map.getTileId((int)this.posX/32, (int)(this.posY/32)-1, moveableLayerId)!=0){
+                this.path.appendStep((int)(this.posX/32), (int)(this.posY/32)-1);
+                }
+        }
+        public void GoStepDown(TiledMap map, int moveableLayerId){
+                if(map.getTileId((int)this.posX/32, (int)(this.posY/32)+1, moveableLayerId)!=0){
+                this.path.appendStep((int)(this.posX/32), (int)(this.posY/32)+1);
+                }
         }
 }
